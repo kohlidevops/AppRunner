6b3c2866d2f89955dc26cdcae3f9fa0f5999cbe4
kohlidevops <100069489+kohlidevops@users.noreply.github.com>
2023-03-26 12:53:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hi Latchu Welcome to Cloud&DevOps, " + name + "!\n"
+    message = "Hi Lakshminarayanan Welcome to Cloud&DevOps, " + name + "!\n"
     return Response(message)
 
 if __name__ == '__main__':
